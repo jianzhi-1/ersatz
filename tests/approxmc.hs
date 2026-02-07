084f3af9c6a314c789a37a42265cc14b2d381989
jianzhi-1 <jianzhiw00@gmail.com>
2026-02-07 16:59:44 -0500

    add template test files for tomorrow

---
+++ tests/approxmc.hs
@@ -0,0 +1,10 @@
+import Ersatz
+import Ersatz.Bit
+import Ersatz.Variable (exists)
+
+import System.Environment (getArgs)
+import Control.Monad ( forM_, replicateM )
+
+main :: IO ()
+main = do
+  -- TODO
